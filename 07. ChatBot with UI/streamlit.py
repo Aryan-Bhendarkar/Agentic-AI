d436b73b6e98fa763f2db028346ba25c4a7ac9a0
Aryan Bhendarkar <bhendarkararyan@gmail.com>
2026-06-08 14:57:32 +0530

    Fixed Pylance Error

--- 07. ChatBot with UI/streamlit.py
+++ 07. ChatBot with UI/streamlit.py
@@ -19,7 +19,7 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG) # type: ignore
     
     ai_message = response['messages'][-1].content
    
