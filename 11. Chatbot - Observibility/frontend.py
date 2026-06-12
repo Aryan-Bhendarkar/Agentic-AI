2976c8771d6029a67c3355a4bb8ed6e8c0434485
Aryan Bhendarkar <bhendarkararyan@gmail.com>
2026-06-12 21:55:26 +0530

    Integrate Observibility and Threads in the Chatbot

--- 11. Chatbot - Observibility/frontend.py
+++ 11. Chatbot - Observibility/frontend.py
@@ -74,12 +74,19 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-       
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn"
+    }
+
     with st.chat_message('assistant'):
         ai_message =  st.write_stream(
            message_chunk.content for message_chunk, metadata in chatbot.stream( # type: ignore
                {"messages": [HumanMessage(content=user_input)]},
-               config={"configurable": {"thread_id": st.session_state["thread_id"]}},
+               config=CONFIG,
                stream_mode='messages'
            )
        )
